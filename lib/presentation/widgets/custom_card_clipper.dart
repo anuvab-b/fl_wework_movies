0bd2375527b05b0d248992f03c26014eb4e3e72a
anuvab-b <anuvabbiswas12@gmail.com>
2024-04-28 14:58:10 +0530

    Widget dimensions refactor

--- lib/presentation/widgets/custom_card_clipper.dart
+++ lib/presentation/widgets/custom_card_clipper.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
 class CustomCardClipper extends CustomClipper<Path> {
-  final BuildContext context;
+  final double scaleFactor;
 
-  CustomCardClipper({required this.context});
+  CustomCardClipper({required this.scaleFactor});
 
   @override
   Path getClip(Size size) {
     var path = Path();
 
-    double scaleFactor = (MediaQuery.of(context).size.width - 2 * 16) / 7;
-
     path.moveTo(0, scaleFactor);
 
     /// Basic Custom Shape
+
     path.lineTo(0, scaleFactor);
     path.lineTo(scaleFactor * 2.5, scaleFactor);
     path.quadraticBezierTo(
@@ -23,16 +22,16 @@ class CustomCardClipper extends CustomClipper<Path> {
     path.lineTo(scaleFactor * 6.5, 0);
     path.quadraticBezierTo(
         scaleFactor * 7, 0, scaleFactor * 7, scaleFactor / 2);
-    path.lineTo(scaleFactor * 7, scaleFactor * 1.5);
+    path.lineTo(scaleFactor * 7, scaleFactor * 1.0);
     path.quadraticBezierTo(
-        scaleFactor * 7, scaleFactor * 2, scaleFactor * 6.5, scaleFactor * 2);
+        scaleFactor * 7, scaleFactor * 1.5, scaleFactor * 6.5, scaleFactor * 1.5);
     path.quadraticBezierTo(
-        scaleFactor * 6, scaleFactor * 2, scaleFactor * 6, scaleFactor * 2.5);
-    path.lineTo(scaleFactor * 6, scaleFactor * 3.5);
+        scaleFactor * 6, scaleFactor * 1.5, scaleFactor * 6, scaleFactor * 2.0);
+    path.lineTo(scaleFactor * 6, scaleFactor * 2.0);
     path.quadraticBezierTo(
-        scaleFactor * 6, scaleFactor * 4, scaleFactor * 5.5, scaleFactor * 4);
-    path.lineTo(scaleFactor / 2, scaleFactor * 4);
-    path.quadraticBezierTo(0, scaleFactor * 4, 0, scaleFactor * 3.5);
+        scaleFactor * 6, scaleFactor * 2.5, scaleFactor * 5.5, scaleFactor * 2.5);
+    path.lineTo(scaleFactor / 2, scaleFactor * 2.5);
+    path.quadraticBezierTo(0, scaleFactor * 2.5, 0, scaleFactor * 2.0);
     path.lineTo(0, scaleFactor * 1.5);
     path.quadraticBezierTo(0, scaleFactor, scaleFactor / 2, scaleFactor);
     path.lineTo(scaleFactor / 2, scaleFactor);
