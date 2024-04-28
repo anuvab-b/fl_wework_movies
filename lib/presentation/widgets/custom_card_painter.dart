f11a90e5cf6fcf5c31e13dfa5af69255a8368390
anuvab-b <anuvabbiswas12@gmail.com>
2024-04-28 14:49:45 +0530

    Minor scale refactor

--- lib/presentation/widgets/custom_card_painter.dart
+++ lib/presentation/widgets/custom_card_painter.dart
@@ -13,7 +13,8 @@ class CustomCardPainter extends CustomPainter {
 
     var path = Path();
 
-    double horizontalScaleFactor = (MediaQuery.of(context).size.width - 2 * 16) / 7;
+    double scaleFactor = (MediaQuery.of(context).size.width - 2 * 16) / 7;
+    double horizontalScaleFactor = scaleFactor;
     double verticalScaleFactor = 10;
 
     /// Square with round edges
@@ -36,13 +37,13 @@ class CustomCardPainter extends CustomPainter {
     path.quadraticBezierTo(30,0,35,0);
     path.lineTo(65,0);
     path.quadraticBezierTo(70,0,70,5);
-    path.lineTo(70,15);
-    path.quadraticBezierTo(70,20,65,20);
-    path.quadraticBezierTo(60,20,60,25);
-    path.lineTo(60,35);
-    path.quadraticBezierTo(60,40,55,40);
-    path.lineTo(5,40);
-    path.quadraticBezierTo(0,40,0,35);
+    path.lineTo(70,10);
+    path.quadraticBezierTo(70,15,65,15);
+    path.quadraticBezierTo(60,15,60,20);
+    path.lineTo(60,20);
+    path.quadraticBezierTo(60,25,55,25);
+    path.lineTo(5,25);
+    path.quadraticBezierTo(0,25,0,20);
     path.lineTo(0,15);
     path.quadraticBezierTo(0,10,5,10);
     path.lineTo(5,10);
@@ -60,16 +61,16 @@ class CustomCardPainter extends CustomPainter {
     // path.lineTo(scaleFactor * 6.5, 0);
     // path.quadraticBezierTo(
     //     scaleFactor * 7, 0, scaleFactor * 7, scaleFactor / 2);
-    // path.lineTo(scaleFactor * 7, scaleFactor * 1.5);
+    // path.lineTo(scaleFactor * 7, scaleFactor * 1.0);
     // path.quadraticBezierTo(
-    //     scaleFactor * 7, scaleFactor * 2, scaleFactor * 6.5, scaleFactor * 2);
+    //     scaleFactor * 7, scaleFactor * 1.5, scaleFactor * 6.5, scaleFactor * 1.5);
     // path.quadraticBezierTo(
-    //     scaleFactor * 6, scaleFactor * 2, scaleFactor * 6, scaleFactor * 2.5);
-    // path.lineTo(scaleFactor * 6, scaleFactor * 3.5);
+    //     scaleFactor * 6, scaleFactor * 1.5, scaleFactor * 6, scaleFactor * 2.0);
+    // path.lineTo(scaleFactor * 6, scaleFactor * 2.0);
     // path.quadraticBezierTo(
-    //     scaleFactor * 6, scaleFactor * 4, scaleFactor * 5.5, scaleFactor * 4);
-    // path.lineTo(scaleFactor / 2, scaleFactor * 4);
-    // path.quadraticBezierTo(0, scaleFactor * 4, 0, scaleFactor * 3.5);
+    //     scaleFactor * 6, scaleFactor * 2.5, scaleFactor * 5.5, scaleFactor * 2.5);
+    // path.lineTo(scaleFactor / 2, scaleFactor * 2.5);
+    // path.quadraticBezierTo(0, scaleFactor * 2.5, 0, scaleFactor * 2.0);
     // path.lineTo(0, scaleFactor * 1.5);
     // path.quadraticBezierTo(0, scaleFactor, scaleFactor / 2, scaleFactor);
     // path.lineTo(scaleFactor / 2, scaleFactor);
