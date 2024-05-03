85215f4750c8bcbbf9b4b4ffbc5e4aaf0cde6e4f
anuvab-b <anuvabbiswas12@gmail.com>
2024-05-03 14:37:02 +0530

    Cache Now Playing response

--- lib/infrastructure/movie_repository.dart
+++ lib/infrastructure/movie_repository.dart
@@ -7,66 +7,83 @@ import 'package:fl_wework_movies/utils/api_endpoints.dart';
 import 'package:fl_wework_movies/utils/secrets.dart';
 import 'package:flutter/material.dart';
 
-class MovieRepository extends IMovieRepository{
+class MovieRepository extends IMovieRepository {
   ApiHelper apiHelper;
+
   MovieRepository({required this.apiHelper});
+
   @override
   Future<Either<String, List<NowPlaying>>> fetchNowPlayingMovies() async {
-    TmdbNowPlayingMovieResponseModel nowPlayingModel;
-    String url = "${ApiEndpoints.tmdbBaseUrl}movie/now_playing";
-    String token = Secrets.tmdbToken;
-
     try {
-      var res = await apiHelper.request(url: url, headers: {
-        "Authorization": "Bearer $token"
-      }, method: HTTPMETHOD.GET);
-      if (res.statusCode == 200) {
-        nowPlayingModel = TmdbNowPlayingMovieResponseModel.fromJson(res.data);
-        return right(nowPlayingModel.results);
+      final database =
+          await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+      final movieDao = database.movieDao;
+      final List<NowPlaying> nowPlaying =
+          await movieDao.getAllNowPlayingMovies();
+      TmdbNowPlayingMovieResponseModel nowPlayingModel;
+      String url = "${ApiEndpoints.tmdbBaseUrl}movie/now_playing";
+      String token = Secrets.tmdbToken;
+      var res = await apiHelper.request(
+          url: url,
+          headers: {"Authorization": "Bearer $token"},
+          method: HTTPMETHOD.GET);
+
+      if (nowPlaying.isNotEmpty) {
+        debugPrint("Returning NowPlaying Cache");
+        return right(nowPlaying);
+      } else if (nowPlaying.isEmpty) {
+        var res = await apiHelper.request(
+            url: url,
+            headers: {"Authorization": "Bearer $token"},
+            method: HTTPMETHOD.GET);
+        if (res.statusCode == 200) {
+          debugPrint("Returning NowPlaying Remote");
+          nowPlayingModel = TmdbNowPlayingMovieResponseModel.fromJson(res.data);
+          movieDao.insertNowPlayingMovies(nowPlayingModel.results);
+          return right(nowPlayingModel.results);
+        } else {
+          return left(res.message);
+        }
+      } else {
+        return right([]);
       }
-      else {
-        return left(res.message);
-      }
-    }
-    catch(e){
+    } catch (e) {
       return left(e.toString());
     }
   }
 
   @override
   Future<Either<String, List<TopRated>>> fetchTopRatedMovies() async {
-
     try {
-      final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+      final database =
+          await $FloorAppDatabase.databaseBuilder('app_database.db').build();
       final movieDao = database.movieDao;
-      final List<TopRated> topRatedMovies = await movieDao.getAllTopRatedMovies();
+      final List<TopRated> topRatedMovies =
+          await movieDao.getAllTopRatedMovies();
       TmdbTopRatedMovieResponseModel topRatedModel;
       String url = "${ApiEndpoints.tmdbBaseUrl}movie/top_rated";
       String token = Secrets.tmdbToken;
 
-      if(topRatedMovies.isNotEmpty) {
+      if (topRatedMovies.isNotEmpty) {
         debugPrint("Returning TopRated Cache");
         return right(topRatedMovies);
-      }
-      else if(topRatedMovies.isEmpty) {
-        var res = await apiHelper.request(url: url, headers: {
-          "Authorization": "Bearer $token"
-        }, method: HTTPMETHOD.GET);
+      } else if (topRatedMovies.isEmpty) {
+        var res = await apiHelper.request(
+            url: url,
+            headers: {"Authorization": "Bearer $token"},
+            method: HTTPMETHOD.GET);
         if (res.statusCode == 200) {
           debugPrint("Returning TopRated Remote");
           topRatedModel = TmdbTopRatedMovieResponseModel.fromJson(res.data);
           movieDao.insertTopRatedMovies(topRatedModel.results);
           return right(topRatedModel.results);
-        }
-        else {
+        } else {
           return left(res.message);
         }
-      }
-      else{
+      } else {
         return right([]);
       }
-    }
-    catch(e){
+    } catch (e) {
       return left(e.toString());
     }
   }
